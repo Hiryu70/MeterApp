9013504fe6b97e0d62a541445494eb79e9592bf6
Hiryu70 <che.maxv@gmail.com>
2018-12-20 08:26:30 +0700

    Add basic entities

---
+++ MeterApp/Meter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeterApp
+{
+	class Meter
+	{
+		public string Name { get; set; }
+
+		public ServiceCategory Service { get; set; }
+
+		public DeviceType DeviceType { get; set; }
+	}
+
+	public enum ServiceCategory
+	{
+
+	}
+
+	public enum DeviceType
+	{
+
+	}
+}
