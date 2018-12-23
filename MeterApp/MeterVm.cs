8f5198a5092b20f3c96a8958e034d15e9b805de2
Hiryu70 <che.maxv@gmail.com>
2018-12-23 17:42:21 +0700

    Files formated

--- MeterApp/MeterVm.cs
+++ MeterApp/MeterVm.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MeterApp
+﻿namespace MeterApp
 {
-	public class MeterVm
+	public sealed class MeterVm
 	{
 		public string Name { get; set; }
 
 		public ServiceCategory Service { get; set; }
 
 		public DeviceType DeviceType { get; set; }
-
-		
 	}
 
 	public enum ServiceCategory
 	{
 		Electric,
-		Water,
-		Gas
+		Water
 	}
 
 	public enum DeviceType
--- MeterApp/MetersManager.cs
+++ MeterApp/MetersManager.cs
@@ -1,9 +1,5 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GalaSoft.MvvmLight.Command;
 
 namespace MeterApp
 {
